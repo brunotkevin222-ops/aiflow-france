c1cdf17c1efd99bee36e837057db7271bae8fd60
brunotkevin222-ops <brunotkevin222@gmail.com>
2026-06-25 19:11:12 +0200

    Create PositioningSection.jsx

---
+++ src/components/playbook/PositioningSection.jsx
@@ -0,0 +1,4 @@
+import React from "react";
+import { SectionHeader } from "../../pages/Playbook";
+const PositioningSection = () => (<div><SectionHeader index="01" title="Positionnement" subtitle="PME françaises 10-200 salariés." /><div className="p-8 bg-gray-50 border border-gray-200 text-center text-gray-500"><p className="text-lg font-semibold">Positionnement</p><p className="mt-2">PME françaises 10-200 salariés.</p></div></div>);
+export default PositioningSection;
