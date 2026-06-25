989d542559c42892999d200e6386490a9022ddee
brunotkevin222-ops <brunotkevin222@gmail.com>
2026-06-25 19:06:01 +0200

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import PublicSite from "./pages/PublicSite";
+import Playbook from "./pages/Playbook";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<PublicSite />} />
+        <Route path="/playbook/*" element={<Playbook />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
