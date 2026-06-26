2a3005759c741c374471cb4e83a2b44894ffbf25
brunotkevin222-ops <brunotkevin222@gmail.com>
2026-06-26 03:40:49 +0200

    Create AutomationsSection.jsx

---
+++ src/components/playbook/AutomationsSection.jsx
@@ -0,0 +1,4 @@
+import React from "react";
+import { SectionHeader } from "../../pages/Playbook";
+const AutomationsSection = () => (<div><SectionHeader index="06" title="Automatisations n8n/Make" subtitle="Workflows CRM, facturation, reporting, onboarding client." /></div>);
+export default AutomationsSection;
