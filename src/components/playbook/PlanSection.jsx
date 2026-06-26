9b766f40cb92bc2b074b1418f91e4fd3a5e64f98
brunotkevin222-ops <brunotkevin222@gmail.com>
2026-06-26 03:42:18 +0200

    Create PlanSection.jsx

---
+++ src/components/playbook/PlanSection.jsx
@@ -0,0 +1,4 @@
+import React from "react";
+import { SectionHeader } from "../../pages/Playbook";
+const PlanSection = () => (<div><SectionHeader index="08" title="Plan 90 jours" subtitle="Semaine 1-2 setup, semaine 3-6 lancement, mois 3 scale." /></div>);
+export default PlanSection;
