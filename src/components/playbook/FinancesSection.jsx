d22ce20228085c9dc1a2bcafc14656ec3e0f7442
brunotkevin222-ops <brunotkevin222@gmail.com>
2026-06-26 03:43:09 +0200

    Create FinancesSection.jsx

---
+++ src/components/playbook/FinancesSection.jsx
@@ -0,0 +1,4 @@
+import React from "react";
+import { SectionHeader } from "../../pages/Playbook";
+const FinancesSection = () => (<div><SectionHeader index="09" title="Finances et tarification" subtitle="Pricing, marges, cashflow, facturation client." /></div>);
+export default FinancesSection;
