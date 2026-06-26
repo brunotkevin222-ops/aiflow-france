ee6f8dced79f3b44b5f5c140356e1601517645bb
brunotkevin222-ops <brunotkevin222@gmail.com>
2026-06-26 03:37:08 +0200

    Create OffersSection.jsx

---
+++ src/components/playbook/OffersSection.jsx
@@ -0,0 +1,4 @@
+import React from "react";
+import { SectionHeader } from "../../pages/Playbook";
+const OffersSection = () => (<div><SectionHeader index="02" title="Nos Offres" subtitle="Starter 890EUR, Growth 1490EUR, Scale 2990EUR." /></div>);
+export default OffersSection;
