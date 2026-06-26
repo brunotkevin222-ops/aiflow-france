8162a5ee63b806442f50d62f29d91a23d11afc36
brunotkevin222-ops <brunotkevin222@gmail.com>
2026-06-26 03:40:01 +0200

    Create ScriptsSection.jsx

---
+++ src/components/playbook/ScriptsSection.jsx
@@ -0,0 +1,4 @@
+import React from "react";
+import { SectionHeader } from "../../pages/Playbook";
+const ScriptsSection = () => (<div><SectionHeader index="05" title="Scripts de vente" subtitle="Prompts cold email, LinkedIn, appels, objections, closing." /></div>);
+export default ScriptsSection;
