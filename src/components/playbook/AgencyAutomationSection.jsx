7ba748bdad78bc180d2c339f57288d012e9c4151
brunotkevin222-ops <brunotkevin222@gmail.com>
2026-06-26 03:44:03 +0200

    Create AgencyAutomationSection.jsx

---
+++ src/components/playbook/AgencyAutomationSection.jsx
@@ -0,0 +1,4 @@
+import React from "react";
+import { SectionHeader } from "../../pages/Playbook";
+const AgencyAutomationSection = () => (<div><SectionHeader index="10" title="Auto. Agence" subtitle="Automatiser la gestion interne de votre agence IA." /></div>);
+export default AgencyAutomationSection;
