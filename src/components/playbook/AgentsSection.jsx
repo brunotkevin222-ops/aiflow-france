6e40a149f78e617545e705ec2c71f49231699d15
brunotkevin222-ops <brunotkevin222@gmail.com>
2026-06-26 03:38:16 +0200

    Create AgentsSection.jsx

---
+++ src/components/playbook/AgentsSection.jsx
@@ -0,0 +1,4 @@
+import React from "react";
+import { SectionHeader } from "../../pages/Playbook";
+const AgentsSection = () => (<div><SectionHeader index="03" title="10 Agents IA" subtitle="Prospection, support, RDV, relance, facturation, reporting, onboarding, FAQ, contenu, analytics." /></div>);
+export default AgentsSection;
