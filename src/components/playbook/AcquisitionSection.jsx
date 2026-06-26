d1f6b201a5556076d1dea732d662afab1f6ff213
brunotkevin222-ops <brunotkevin222@gmail.com>
2026-06-26 03:39:11 +0200

    Create AcquisitionSection.jsx

---
+++ src/components/playbook/AcquisitionSection.jsx
@@ -0,0 +1,4 @@
+import React from "react";
+import { SectionHeader } from "../../pages/Playbook";
+const AcquisitionSection = () => (<div><SectionHeader index="04" title="Acquisition" subtitle="Cold email, LinkedIn, contenu, SEO, referral." /></div>);
+export default AcquisitionSection;
