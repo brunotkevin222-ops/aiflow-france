d4090bc05f92b8772a41d1a4335534f244badd5e
brunotkevin222-ops <brunotkevin222@gmail.com>
2026-06-26 03:41:34 +0200

    Create WebsiteSection.jsx

---
+++ src/components/playbook/WebsiteSection.jsx
@@ -0,0 +1,4 @@
+import React from "react";
+import { SectionHeader } from "../../pages/Playbook";
+const WebsiteSection = () => (<div><SectionHeader index="07" title="Site web et contenus" subtitle="Landing page, cas clients, newsletter, social proof." /></div>);
+export default WebsiteSection;
